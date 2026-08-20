639ae6dcfe7532202042814fa2b80e5371c1f469
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:07:50 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-numeric-finite-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -22,8 +22,14 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-finite-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-numeric-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-finite-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-numeric-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
@@ -36,7 +42,7 @@ let package = Package(
         .target(
             name: "Numeric Finite Primitives Test Support",
             dependencies: [
-                "Numeric Finite Primitives",
+                "Numeric Finite Primitives"
             ],
             path: "Tests/Support"
         ),
